1e115b8ed461b2e91e697e7b0ef6bca2ef12329b
Johnny Vardel <80030559+lemonadebusinesssolutions@users.noreply.github.com>
2025-04-19 11:33:57 +0100

    Add models/User.js 1745058834920

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  resetPasswordToken: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  resetPasswordExpires: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = { User };
